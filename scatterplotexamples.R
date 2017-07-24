a5868003b40b12a1f1a379abe74d49176cf44d07
beronicao <beronica.ocasio@gmail.com>
2017-07-24 14:15:10 -0400

    updated scatterplotexamples.R

--- scatterplotexamples.R
+++ scatterplotexamples.R
@@ -1,9 +1,11 @@
 #to generate 2 random plots
+attach(mtcars)
 par(mfrow=c(1,2))
 plot(wt, mpg, main="Scatterplot Example", 
             xlab="Car Weight ", ylab="Miles Per Gallon ", pch=19)
 plot(wt, hp, main= "Scatterplot Example 2", xlab="Car Weight", ylab="HP", pch=19)
 
+mtcars <- mtcars
 
 # second way: add dots on bottom and left panel
   # before the scatters are drawn, 
@@ -30,7 +32,7 @@ plot(wt, hp, main= "Scatterplot Example 2", xlab="Car Weight", ylab="HP", pch=19
                                        colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75))
                   )
  
- 
+ ######
  with(quakes, scatter3D(x = long, y = lat, z = -depth, colvar = mag, 
                                pch = 16, cex = 1.5, xlab = "longitude", ylab = "latitude", 
                                zlab = "depth, km", clab = c("Richter","Magnitude"),
@@ -38,4 +40,6 @@ plot(wt, hp, main= "Scatterplot Example 2", xlab="Car Weight", ylab="HP", pch=19
                                type = "h", theta = 10, d = 2, 
                                colkey = list(length = 0.5, width = 0.5, cex.clab = 0.75))
              )
+ quakes <- quakes
+ #####
 
